87db0009b85bc98febe3fe5a71586ef73f95f8d2
Abdelrahman <abdulrahman7ossam@Gmail.com>
2020-08-11 02:04:36 +0300

    Init pages and created routes.dart file

---
+++ lib/routes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+import 'package:blood_donation/pages/homePage/homePage.dart';
+import 'package:blood_donation/pages/information/information.dart';
+import 'package:blood_donation/pages/introduction/introduction.dart';
+import 'package:blood_donation/pages/profile/profile.dart';
+import 'package:blood_donation/pages/registration/registration.dart';
+
+/// Routes as Map<String, WidgetBuilder>.
+/// Used for the route parameter in MaterialApp.
+final Map<String, WidgetBuilder> routesAsWidgetBuilder =
+    <String, WidgetBuilder>{
+  '/HomePage': (context) => HomePage(),
+  '/Information': (context) => Information(),
+  '/Introduction': (context) => Introduction(),
+  '/Profile': (context) => Profile(),
+  '/Registration': (context) => Registration(),
+};
