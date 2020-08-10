239f2a681512193c7cf99c075b1572e8809405c8
Abdelrahman <abdulrahman7ossam@Gmail.com>
2020-08-11 02:49:40 +0300

    Made Login Pager Header.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:blood_donation/routes.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      routes: routesAsWidgetBuilder,
       theme: ThemeData(fontFamily: "ProximaNova"),
+      initialRoute: '/Registration',
       home: MyApp(),
     ),
   );
