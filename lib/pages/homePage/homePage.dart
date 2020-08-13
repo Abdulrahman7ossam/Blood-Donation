cea77cc1b40dad0756fdc3a8774a4d1ee739a3bd
Abdelrahman <abdulrahman7ossam@Gmail.com>
2020-08-13 08:00:33 +0300

    Added home page top card.

--- lib/pages/homePage/homePage.dart
+++ lib/pages/homePage/homePage.dart
@@ -11,9 +11,149 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          BackgroundGradient(),
+          const BackgroundGradient(),
+          Column(
+            children: [
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _NumberOfDonations(),
+                  _NumberOfTimesBloodWasUsed(),
+                ],
+              ),
+            ],
+          ),
+          _NextDonationWindow(),
+          _ScheduleAnApointmentButton(),
         ],
       ),
+    );
+  }
+}
+
+class _NumberOfTimesBloodWasUsed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Lives Saved",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+          ),
+        ),
+        Text(
+          "0",
+          style: TextStyle(
+            color: Colors.green,
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _NumberOfDonations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Donations",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+          ),
+        ),
+        Text(
+          "0",
+          style: TextStyle(
+            color: Colors.green,
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _NextDonationWindow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(height: 250),
+        Text(
+          "Donation Window : ",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(width: 5),
+        Text(
+          "Now!",
+          style: TextStyle(
+            color: Colors.green,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ScheduleAnApointmentButton extends StatefulWidget {
+  @override
+  __ScheduleAnApointmentButtonState createState() =>
+      __ScheduleAnApointmentButtonState();
+}
+
+class __ScheduleAnApointmentButtonState
+    extends State<_ScheduleAnApointmentButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            SizedBox(height: 170),
+            FlatButton(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "DONATE",
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              onPressed: () {
+                print("Donation button pressed!");
+              },
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                side: BorderSide(color: Colors.white, width: 4.0),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
@@ -25,7 +165,12 @@ class BackgroundGradient extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 250.0,
+      width: double.infinity,
       decoration: BoxDecoration(
+        // borderRadius: BorderRadius.only(
+        //   bottomLeft: Radius.circular(20),
+        //   bottomRight: Radius.circular(20),
+        // ),
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
