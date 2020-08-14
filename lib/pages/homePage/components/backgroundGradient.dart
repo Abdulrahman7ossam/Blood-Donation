9f97674d7b18f77533d9daae5940be2c0f83b707
Abdelrahman <abdulrahman7ossam@Gmail.com>
2020-08-15 02:20:55 +0300

    Added video card to homepage.

--- lib/pages/homePage/components/backgroundGradient.dart
+++ lib/pages/homePage/components/backgroundGradient.dart
@@ -9,10 +9,6 @@ class BackgroundGradient extends StatelessWidget {
       height: 260.0,
       width: double.infinity,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(25),
-          bottomRight: Radius.circular(25),
-        ),
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
--- lib/pages/homePage/homePage.dart
+++ lib/pages/homePage/homePage.dart
@@ -2,6 +2,7 @@ import 'package:blood_donation/pages/homePage/components/backgroundGradient.dart
 import 'package:blood_donation/pages/homePage/components/donateButton.dart';
 import 'package:blood_donation/pages/homePage/components/donationsLivesSaved.dart';
 import 'package:blood_donation/pages/homePage/components/nextDonationWindow.dart';
+import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -21,10 +22,64 @@ class _HomePageState extends State<HomePage> {
               children: [
                 const LastDonationCard(),
                 const FactOfTheDay(),
+                const VideoDecoration(),
               ],
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class VideoDecoration extends StatefulWidget {
+  /// Video Decoration card at the bottom of the homepage.
+  const VideoDecoration();
+
+  @override
+  _VideoDecorationState createState() => _VideoDecorationState();
+}
+
+class _VideoDecorationState extends State<VideoDecoration> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        VideoPlayerController.asset('assets/donation_stop_motion_video.mp4')
+          ..initialize().then((_) {
+            // Ensure the first frame is shown after the video is initialized,
+            // even before the play button has been pressed.
+            setState(() {
+              // mute, loop, and play the video.
+              _controller.setVolume(0);
+              _controller.setLooping(true);
+              _controller.play();
+            });
+          });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Container(
+        height: 200,
+        alignment: Alignment.center,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15.0),
+          child: VideoPlayer(_controller),
+        ),
       ),
     );
   }
@@ -76,7 +131,7 @@ class FactOfTheDay extends StatelessWidget {
         fit: BoxFit.cover,
         alignment: Alignment.topCenter,
         colorFilter: ColorFilter.mode(
-          Colors.black.withOpacity(0.6),
+          Colors.black.withOpacity(0.7),
           BlendMode.dstATop,
         ),
       ),
